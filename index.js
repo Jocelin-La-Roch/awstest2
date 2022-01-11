6eb998c850fd7d93def715e9ab3c7c3a89790d77
Jocelin-La-Roch <jocelinlaroch08@gmail.com>
2022-01-11 21:41:47 +0100

    init

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const http = require("http");
 
 const port = process.env.port || 3000;
 
@@ -9,8 +8,6 @@ app.get('/', (req, res, next) => {
     res.send("** MAITRE YODA VOUS SALUT **");
 });
 
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log("** MAITRE YODA * IS CONNECTED**");
+app.listen(port, () => {
+    console.log("** MAITRE YODA * IS CONNECTED **");
 });
